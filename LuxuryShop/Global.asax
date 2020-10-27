b8bc053d75aeea58375fe471cd29f8df495aaf31
harinam198 <lehainamx1@gmail.com>
2020-10-27 20:34:17 +0700

    re

--- LuxuryShop/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LuxuryShop.MvcApplication" Language="C#" %>
---
+++ luxuryShop/luxuryShop/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using luxuryShop.Models.Enity;
+using luxuryShop.Views.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace luxuryShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public luxuryEntities db = new luxuryEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DienThoai()
+        {
+            var Product = new Category();
+            ViewBag.Category = Product.Dienthoai();
+            return View();
+        }
+        public ActionResult Tainghe()
+        {
+            var Product = new Category();
+            ViewBag.Category = Product.Tainghe();
+            return View();
+        }
+        public ActionResult DongHo()
+        {
+            var Product = new Category();
+            ViewBag.Category = Product.Dongho();
+            return View();
+        }
+
+    }
+}
